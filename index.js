fffbb5e6f5716c108d77925969120a6521541b59
farish-jamal <farishjamal98@gmail.com>
2024-02-07 01:20:56 +0530

    Database connection

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+const handleConnectToDatabase =  require("./db/connection.db");
 
 const PORT = process.env.PORT || 3001;
+
+handleConnectToDatabase(process.env.MONGODB_URL).then(()=>{
+ console.log("Connected to MongoDB successfully");
+})
 
 app.get("/", (req, res)=> {
  return res.send("Hello World!");
 })
 
 app.listen(PORT, ()=>{
- console.log(`Server Running On Port ${PORT}`);
+ console.log(`Server running on Port ${PORT}`);
 })
