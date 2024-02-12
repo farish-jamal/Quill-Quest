acd6eef4d093196d682cabe0b46829f805d056de
farish-jamal <farishjamal98@gmail.com>
2024-02-13 01:59:07 +0530

    test push

--- src/db/connection.db.js
+++ src/db/connection.db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 async function handleConnectToDatabase(url) {
   return mongoose.connect(url);
+  // Database connection
 }
 
 module.exports = handleConnectToDatabase;
